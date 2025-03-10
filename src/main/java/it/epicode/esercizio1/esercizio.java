1d3fdc71014b03f87b8656ebc44f9609fe3aa19c
SilviaFranzinelli <franzinellisilvia@gmail.com>
2025-03-10 17:51:57 +0100

    1

---
+++ src/main/java/it/epicode/esercizio1/esercizio.java
@@ -0,0 +1,45 @@
+package it.epicode.esercizio1;
+
+public class esercizio {
+    // 1 metodo "prodotto": accetta due numeri interi e ritorna il loro prodotto
+
+    public int prodotto(int a, int b) {
+        return a * b;
+    }
+
+    // 2 metodo "concatena": accetta una stringa e un intero e restituisce una stringa che concatena gli elementi
+
+    public String concatena(String a, int b) {
+        return a + b;
+    }
+
+    // 3 metodo "inserisciInArray": accetta un array di strighe di cinque elementi ed una stringa che restituisca un array di sei elementi in cui la stringa passata sia al terzo posto e le stringhe precedenti in quarta e quinta posizione siano rispettivamente in quinta e sesta posizione
+
+    public static void inserisciInArray(String[] arr, String s) {
+        String[] stringa = new String[6];
+        stringa[0] = 1;
+        stringa[1] = 2;
+        stringa[2] = s;
+        stringa[3] = 4;
+        stringa[4] = 5;
+        stringa[5] = 6;
+
+        String[] stringa2 = new String[6];
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
